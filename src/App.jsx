4fa70cd65702f2847f80094a24519a5e994e66bf
pranesh12 <praneshck7@gmail.com>
2024-02-08 11:06:48 +0600

    add todo added

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ import Todo from "./components/Todo/Todo";
 export const TodoContext = createContext(null);
 
 function App() {
-  const [completedTodo, setCompletedTodo] = useState("hello");
-  const [unCompletedTodo, setUnCompletedTodo] = useState([]);
+  const [todos, setTodos] = useState([
+    { id: "1", taskName: "Eat rice", completed: false, priority: "low" },
+    { id: "2", taskName: "Go to Gym", completed: true, priority: "medium" },
+    { id: "3", taskName: "Read 1 book", completed: false, priority: "high" },
+  ]);
 
   return (
     <div className="container">
       <TodoContext.Provider
         value={{
-          completedTodo,
-          setCompletedTodo,
-          unCompletedTodo,
-          setUnCompletedTodo,
+          todos,
+          setTodos,
         }}
       >
         <h1>Hello</h1>
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,12 +1,84 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { TodoContext } from "../../App";
-
+import { v4 as uuidv4 } from "uuid";
 const Todo = () => {
-  const { completedTodo } = useContext(TodoContext);
-  console.log(completedTodo);
+  const { todos, setTodos } = useContext(TodoContext);
+  const [taskValues, setTaskValues] = useState({
+    id: uuidv4(),
+    taskName: "",
+    completed: false,
+    priority: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: taskValues.id,
+      taskName: taskValues.taskName,
+      completed: taskValues.completed,
+      priority: taskValues.priority,
+    };
+    if (newTodo.taskName.trim() !== "") {
+      setTodos([...todos, newTodo]);
+    }
+  };
+  const handleChange = (e) => {
+    setTaskValues({ ...taskValues, [e.target.name]: e.target.value });
+  };
+
+  console.log(taskValues);
   return (
     <div>
       <h1>Todod</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={taskValues.taskName}
+          onChange={handleChange}
+          name="taskName"
+          type="text"
+          placeholder="Add Todo"
+        />
+        <select
+          onChange={handleChange}
+          name="priority"
+          value={taskValues.priority}
+        >
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+        <button type="submit">Add Todo</button>
+      </form>
+
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Todo name</th>
+              <th scope="col">Status</th>
+              <th scope="col">Switch</th>
+              <th scope="col">Edit</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos &&
+              todos.map((todo) => {
+                return (
+                  <tr key={todo.id}>
+                    <th scope="row">1</th>
+                    <td>{todo.taskName}</td>
+                    <td>{todo.priority}</td>
+                    <td>switbutton</td>
+                    <td>editbutton</td>
+                    <td>deleteButton</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
